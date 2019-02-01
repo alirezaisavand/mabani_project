f92a3540e0d77b4a0b33d97568eb2f573fc4c02c
alirezaisavand <45400538+alirezaisavand@users.noreply.github.com>
2019-02-01 21:14:39 +0330

    cmake build debug

---
+++ cmake-build-debug/CMakeFiles/project.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Lenovo/Desktop/university/mabani/project/project/src/logic.c" "C:/Users/Lenovo/Desktop/university/mabani/project/project/cmake-build-debug/CMakeFiles/project.dir/src/logic.c.obj"
+  "C:/Users/Lenovo/Desktop/university/mabani/project/project/src/main.c" "C:/Users/Lenovo/Desktop/university/mabani/project/project/cmake-build-debug/CMakeFiles/project.dir/src/main.c.obj"
+  "C:/Users/Lenovo/Desktop/university/mabani/project/project/src/physics.c" "C:/Users/Lenovo/Desktop/university/mabani/project/project/cmake-build-debug/CMakeFiles/project.dir/src/physics.c.obj"
+  "C:/Users/Lenovo/Desktop/university/mabani/project/project/src/simple_snake.c" "C:/Users/Lenovo/Desktop/university/mabani/project/project/cmake-build-debug/CMakeFiles/project.dir/src/simple_snake.c.obj"
+  "C:/Users/Lenovo/Desktop/university/mabani/project/project/src/view.c" "C:/Users/Lenovo/Desktop/university/mabani/project/project/cmake-build-debug/CMakeFiles/project.dir/src/view.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../sdl2/sdl2-include"
+  "../sdl2/sdl2-gfx-include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/project.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project.dir/src/logic.c.obj"
+  "CMakeFiles/project.dir/src/main.c.obj"
+  "CMakeFiles/project.dir/src/physics.c.obj"
+  "CMakeFiles/project.dir/src/simple_snake.c.obj"
+  "CMakeFiles/project.dir/src/view.c.obj"
+  "project.pdb"
+  "project.exe"
+  "project.exe.manifest"
+  "libproject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
